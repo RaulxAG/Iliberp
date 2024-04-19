2e0f20165531feeac5efed9d69d43ddbac2caa79
RaulxAG <raul.arrgon@outlook.es>
2024-04-19 22:52:42 +0200

    creado proyecto vite REACT, añadido Bootstrap, SASS, variables y estilos generales

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import Inicio  from './Pages/Inicio'
+
+function App() {
+  return (
+    <>
+      <Inicio></Inicio>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Inicio.jsx
@@ -0,0 +1,13 @@
+
+export default function Inicio() {
+    return (
+      <main className="mainInicio">
+        <section className="mainInicio__barraLateral">
+          <section className="barraLateral__profile">
+            <img src="#" alt="Foto Perfil"/>
+            <p>Nombre</p>
+          </section>
+        </section>
+      </main>
+    );
+  }
