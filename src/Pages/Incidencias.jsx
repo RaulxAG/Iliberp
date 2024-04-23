8b83b421cef7a3db7b65b6401a55bb3258393721
RaulxAG <raul.arrgon@outlook.es>
2024-04-23 17:45:15 +0200

    menu lateral reestructurado, movida la carpeta assets a public/, estilos globales reestructurados, pagina incidencias reestructurada, empezado: reestructuramiento de formulario de incidencias

--- src/Pages/Incidencias.jsx
+++ src/Pages/Incidencias.jsx
@@ -1,53 +1,62 @@
 import Menu from "../components/Menu";
 
+
+// contenedor mainIncidencias 
+// contenedor
 export default function Inidencias() {
     return (
-        <main className="contenedor mainIncidencias">
+        <main className="contenedor row">
             <Menu selected="incidencias"></Menu>
-            <form action="/" method="post" className="contenedor">
-                <section className="mainIncidencias__formulario">
-                    <h4>Reportar una incidencia</h4>
+            <form action="/" method="post" className="col-11 p-1 row">
+                <section className="box h-100 col-10 p-1">
+                    <div className="h-100">
+                        <h4 className="tittle my-4">Reportar una incidencia</h4>
 
-                    <div>
-                        <div className="formulario__input">
-                            <label htmlFor="titulo">Motivo de tu incidencia</label>
-                            <input type="text" name="titulo" id="titulo" />
-                        </div>
-                        <div className="formulario__inputGroup">
+                        <div>
                             <div className="formulario__input">
-                                <label htmlFor="categoria">Categoría</label>
-                                <select name="categoria" id="categoria">
-                                    <option value="ciberseguridad">Ciberseguridad</option>
-                                    <option value="programacion">Programacion</option>
-                                    <option value="telefonia">Telefonía</option>
-                                    <option value="sistemas">Sistemas</option>
-                                    <option value="taller">Taller</option>
-                                    <option value="web">Web</option>
-                                </select>
+                                <label htmlFor="titulo">Motivo de tu incidencia</label>
+                                <input type="text" name="titulo" id="titulo" />
+                            </div>
+                            <div className="formulario__inputGroup">
+                                <div className="formulario__input">
+                                    <label htmlFor="categoria">Categoría</label>
+                                    <select name="categoria" id="categoria">
+                                        <option value="ciberseguridad">Ciberseguridad</option>
+                                        <option value="programacion">Programacion</option>
+                                        <option value="telefonia">Telefonía</option>
+                                        <option value="sistemas">Sistemas</option>
+                                        <option value="taller">Taller</option>
+                                        <option value="web">Web</option>
+                                    </select>
+                                </div>
+                                <div className="formulario__input">
+                                    <label htmlFor="telefono">Teléfono</label>
+                                    <input type="tel" name="telefono" id="telefono" />
+                                </div>
+                                <div className="formulario__input">
+                                    <label htmlFor="email">Correo</label>
+                                    <input type="email" name="email" id="email" />
+                                </div>
                             </div>
                             <div className="formulario__input">
-                                <label htmlFor="telefono">Teléfono</label>
-                                <input type="tel" name="telefono" id="telefono" />
+                                <label htmlFor="descripcion">Descripción de tu incidencia:</label>
+                                <textarea name="descripcion" id="descripcion" cols="30" rows="10"></textarea>
                             </div>
-                            <div className="formulario__input">
-                                <label htmlFor="email">Correo</label>
-                                <input type="email" name="email" id="email" />
-                            </div>
-                        </div>
-                        <div className="formulario__input">
-                            <label htmlFor="descripcion">Descripción de tu incidencia:</label>
-                            <textarea name="descripcion" id="descripcion" cols="30" rows="10"></textarea>
                         </div>
                     </div>
+                    
                 </section>
 
-                <section className="formulario__acciones">
-                    <img src="./public/assets/img/logo.png" alt="Logo" />
+                <section className="col-2">
+                    <div className="h-100">
+                        <img width="200" src="/assets/img/logo.png" alt="Logo" />
 
-                    <div>
-                        <input type="submit" value="Enviar" id="submit" />
-                        <input type="reset" value="Vaciar" id="reset" />
+                        <div>
+                            <input type="submit" value="Enviar" id="submit" />
+                            <input type="reset" value="Vaciar" id="reset" />
+                        </div>
                     </div>
+                    
                 </section>
             </form>
         </main>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,43 +3,50 @@
 export default function Menu({ selected }) {
     console.log(selected);
 
+    // menuLateral
     return (
-      <section className="menuLateral">
-        <section className="menuLateral__profile">
-            <img src="./public/assets/img/usuario.png" alt="Foto de perfil" />
-            <p>Usuario</p>
-        </section>
-        <section className="menuLateral__enlaces">
-            <ul>
-                <li className={selected === "inicio" ? "selected" : ""} >
-                    <a href="#">
-                        <i className="fa-solid fa-house"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className={selected === "incidencias" ? "selected" : ""} >
-                        <i className="fa-solid fa-triangle-exclamation"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className={selected === "mensajeria" ? "selected" : ""} >
-                        <i className="fa-solid fa-comments"></i>
-                    </a>
-                </li>
-                <li className={selected === "tienda" ? "selected" : ""} >
-                    <a href="#">
-                        <i className="fa-solid fa-shop"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i className="fa-solid fa-language"></i>
-                    </a>
-                </li>
-            </ul>
-        </section>
-        <section className="menuLateral__logo">
-            <img src="./public/assets/img/logo.png" alt="logo" />
+      <section className="menuLateral col-1 p-1">
+        <section className="box h-100 d-flex flex-column align-items-center justify-content-between">
+            <div>
+                <section className="menuLateral__profile py-3">
+                    <img src="/assets/img/usuario.png" className="profile__photo" alt="Foto de perfil" />
+                    <p>Usuario</p>
+                </section>
+
+                <section className="menuLateral__enlaces">
+                    <ul>
+                        <li className={selected === "inicio" ? "selected" : ""} >
+                            <a href="#">
+                                <i className="fa-solid fa-house"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className={selected === "incidencias" ? "selected" : ""} >
+                                <i className="fa-solid fa-triangle-exclamation"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className={selected === "mensajeria" ? "selected" : ""} >
+                                <i className="fa-solid fa-comments"></i>
+                            </a>
+                        </li>
+                        <li className={selected === "tienda" ? "selected" : ""} >
+                            <a href="#">
+                                <i className="fa-solid fa-shop"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fa-solid fa-language"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+            </div>
+            
+            <section className="menuLateral__logo d-flex align-items-center justify-content-center py-2">
+                <img src="/assets/img/logo.png" alt="logo" />
+            </section>
         </section>
       </section>  
     );
