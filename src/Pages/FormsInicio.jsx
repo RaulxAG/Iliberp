4370f627f409c96d4be3cab4ce1ad4280a134155
judithmf00 <judithmf00@gmail.com>
2024-05-09 20:05:14 +0200

    Formulario inicio sesion con trasicion

--- src/Pages/FormsInicio.jsx
+++ src/Pages/FormsInicio.jsx
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom';
-
+import { useState } from 'react';
 export default function FormInicio() {
+    const [form, setForm] = useState(true);
+
+    function FORMULARIO(direccion) {
+        let formIniciarSesion = document.getElementById("formularios")
+        direccion=="left" ?  formIniciarSesion.style.left = "0" : formIniciarSesion.style.left = "50%";
+        setForm(!form)
+    }
+
     return (
         <div className='containerInicio d-flex'>
             <div className="portadaForms m-auto row justify-content-between position-relative">
@@ -10,39 +18,40 @@ export default function FormInicio() {
                         <i className="fa-solid fa-user" style={{color: "#f8f8f8"}}></i>
                     </div>
                     <p>¿Aún no tienes cuenta? Regístrate</p>
-                    <button>Regístrate</button>
+                    <button onClick={()=>FORMULARIO("left")}>Regístrate</button>
                 </div>
                 <div className="portadaLogin col d-flex flex-column align-items-center">
                     <h3 className="text-uppercase">Iniciar Sesión</h3>
                     <div>
                         <i className="fa-solid fa-user" style={{color: "#f8f8f8"}}></i>
                     </div>
-                    <button>Iniciar Sesión</button>
+                    <button onClick={()=>FORMULARIO("right")}>Iniciar Sesión</button>
                 </div>
 
-                <div className="formularios position-absolute">
-                    {/* <div className="formIniciarSesion">
-                        <h3 className="text-uppercase">Iniciar Sesión</h3>
-                        <form action="" className="d-flex flex-column gap-4 mt-4">
-                            <input type="text" placeholder="Usuario"/>
-                            <input type="text" placeholder="Contraseña"/>
-                        </form>
-                        <p className='mt-4'>¿Has olvidado tu contraseña?</p>
-                        <Link to="/tienda">
+                <div className="formularios position-absolute" id='formularios'>
+                    {form ? 
+                        <div className="formIniciarSesion" id='formIniciarSesion'>
+                            <h3 className="text-uppercase">Iniciar Sesión</h3>
+                            <form action="" className="d-flex flex-column gap-4 mt-4">
+                                <input type="text" placeholder="Usuario"/>
+                                <input type="text" placeholder="Contraseña"/>
+                            </form>
+                            <p className='mt-4'>¿Has olvidado tu contraseña?</p>
                             <button>Iniciar Sesión</button>
-                        </Link>
-                    </div> */}
-                    <div className="formRegistrarse">
-                        <h3 className="text-uppercase">Crear cuenta</h3>
-                        <form action="" className="d-flex flex-column gap-4">
-                            <input type="text" placeholder="Usuario"/>
-                            <input type="text" placeholder="Contraseña"/>
-                            <input type="text" placeholder="Nombre completo"/>
-                            <input type="text" placeholder="Teléfono"/>
-                            <input type="text" placeholder="Email"/>
-                        </form>
-                        <button>Registrarme</button>
-                    </div>
+                        </div>
+                    :
+                        <div className="formRegistrarse">
+                            <h3 className="text-uppercase">Crear cuenta</h3>
+                            <form action="" className="d-flex flex-column gap-4">
+                                <input type="text" placeholder="Usuario"/>
+                                <input type="text" placeholder="Contraseña"/>
+                                <input type="text" placeholder="Nombre completo"/>
+                                <input type="text" placeholder="Teléfono"/>
+                                <input type="text" placeholder="Email"/>
+                            </form>
+                            <button>Registrarme</button>
+                        </div>
+                    } 
                 </div>
             </div>
         </div>
