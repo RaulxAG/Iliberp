a9e9f07f3b068fc980f69528d8568061ae82c611
judithmf00 <judithmf00@gmail.com>
2024-05-13 17:27:47 +0200

    Creados modelos de admnistracion

--- BackEnd/administracion/models.py
+++ BackEnd/administracion/models.py
@@ -2,26 +2,77 @@ from django.db import models
 
 class Empresa(models.Model):
     """
-    Modelo que representa los datos de las sedes de las empresas.
+    Modelo que representa los datos de las empresas clientes
 
     Campos:
-        cifNif (CharField): Numero de identificacion fiscal de la empresa.
-        cantIncident (IntegerField): Total de incidencias que lleva hechas la empresa.
-        cuota (CharField): Cuota que tiene contratada la empresa.
-        nombre (TextField): Nombre de la empresa.
-        telefono (CharField): Número de teléfono de la empresa, se usa char parapermitir el prefijo con +
-        correo (CharField): Correo de la empresa.
+        nombre (CharField): Nombre de la empresa
+        cif_nif (Charfield): Cif o nif (en caso de autónomo) de la empresa
+        direccion (CharField): Dirección de la empresa
+        email (EmailField): Correo electrónico de la empresa.
+        telefono1 (CharField): Número de teléfono principal de la empresa (se usa char para permitir el prefijo con +)
+        telefono2 (CharField): Segundo número de teléfono de la empresa (opcional)
     """
 
-    cifNif = models.CharField(max_length=9,default=0)
-    cantIncident = models.IntegerField(default=0)
-    email = models.CharField(max_length=20)
-    phone1 = models.CharField(max_length=13)
-    phone2 = models.CharField(max_length=13,default=0)
-    time = models.TimeField(default=0,null=True)
-    locality = models.CharField(max_length=50)
-    # BONO por determinar
-    time_left = models.TimeField(default=0, null=True)
+    nombre = models.CharField(max_length=100)
+    cif_nif = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=255)
+    email = models.EmailField(max_length=100)
+    telefono1 = models.CharField(max_length=20)
+    telefono2 = models.CharField(max_length=20, blank=True, null=True)
+    
 
-    enterprise = models.ForeignKey(Enterprise,on_delete=models.CASCADE, related_name='branchs')
+class Cliente(models.Model):
+    """
+    Modelo que representa los datos de los clientes
 
+    Campos:
+        nombre (CharField): Nombre del cliente
+        apellidos (CharField): Apellidos del cliente
+        dni (CharField): DNI del cliente
+        correo (EmailField): Correo electrónico del cliente
+        telefono1 (CharField): Número de teléfono principal del cliente
+        telefono2 (CharField): Segundo número de teléfono del cliente (opcional)
+        foto_perfil (BinaryField): Foto de perfil del cliente (opcional)
+        empresa (ForeignKey): Relación con el modelo Empresa para asociar el cliente con una empresa
+    """
+
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    dni = models.CharField(max_length=20)
+    correo = models.EmailField(max_length=100)
+    telefono1 = models.CharField(max_length=20)
+    telefono2 = models.CharField(max_length=20, blank=True, null=True) 
+    foto_perfil = models.BinaryField(blank=True, null=True) 
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)  # Relación con el modelo Empresa
+    
+class Empleado(models.Model):
+    """
+    Modelo que representa los datos de los empleados
+
+    Campos:
+        nombre (CharField): Nombre del empleado
+        apellidos (CharField): Apellidos del empleado
+        dni (CharField): DNI del empleado
+        correo (EmailField): Correo electrónico del empleado
+        telefono (CharField): Número de teléfono del empleado
+        departamento (CharField): Departamento al que pertenece el empleado
+        foto_perfil (BinaryField, opcional): Foto de perfil del empleado (opcional)
+    """
+
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    dni = models.CharField(max_length=20)
+    correo = models.EmailField(max_length=100)
+    telefono = models.CharField(max_length=20)
+    departamento = models.CharField(max_length=20, choices=DEPARTAMENTOS)
+    foto_perfil = models.BinaryField(blank=True, null=True)
+
+    DEPARTAMENTOS = [
+        ('web', 'Web'),
+        ('programacion', 'Programación'),
+        ('administracion', 'Administración'),
+        ('ciberseguridad', 'Ciberseguridad'),
+        ('telefonia', 'Telefonía'),
+        ('sistemas', 'Sistemas'),
+        ('taller', 'Taller'),
+    ]
