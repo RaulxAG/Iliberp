9578d88b45e2f132212a8c5fd9b18dec49ba74ec
judithmf00 <judithmf00@gmail.com>
2024-05-13 17:05:38 +0200

    Registradas las carpetas de los modulos en setting.py y configuradas la url de ilierp

--- BackEnd/administracion/models.py
+++ BackEnd/administracion/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Empresa(models.Model):
+    """
+    Modelo que representa los datos de las sedes de las empresas.
+
+    Campos:
+        cifNif (CharField): Numero de identificacion fiscal de la empresa.
+        cantIncident (IntegerField): Total de incidencias que lleva hechas la empresa.
+        cuota (CharField): Cuota que tiene contratada la empresa.
+        nombre (TextField): Nombre de la empresa.
+        telefono (CharField): Número de teléfono de la empresa, se usa char parapermitir el prefijo con +
+        correo (CharField): Correo de la empresa.
+    """
+
+    cifNif = models.CharField(max_length=9,default=0)
+    cantIncident = models.IntegerField(default=0)
+    email = models.CharField(max_length=20)
+    phone1 = models.CharField(max_length=13)
+    phone2 = models.CharField(max_length=13,default=0)
+    time = models.TimeField(default=0,null=True)
+    locality = models.CharField(max_length=50)
+    # BONO por determinar
+    time_left = models.TimeField(default=0, null=True)
+
+    enterprise = models.ForeignKey(Enterprise,on_delete=models.CASCADE, related_name='branchs')
+
--- BackEnd/iliberp/settings.py
+++ BackEnd/iliberp/settings.py
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'administracion',
+    'fichaje',
+    'incidencias',
+    'inventario',
+    'mensajeria',
+    'trello',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
