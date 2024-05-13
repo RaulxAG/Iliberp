f683477ee362780aac463e44ac2b608f2c44a39f
judithmf00 <judithmf00@gmail.com>
2024-05-13 17:48:14 +0200

    Creado modelo de inventario

--- BackEnd/inventario/models.py
+++ BackEnd/inventario/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    """
+    Modelo que representa los productos en la tienda.
+
+    Campos:
+        nombre (CharField): Nombre del producto
+        descripcion (TextField): Descripción del producto
+        precio (DecimalField): Precio del producto
+        especificaciones (JSONField): Especificaciones del producto (almacenadas como JSON)
+        foto (BinaryField): Foto del producto (almacenada como datos binarios)
+    """
+
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    especificaciones = models.JSONField()
+    foto = models.BinaryField()
