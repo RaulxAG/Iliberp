09d85d240f675b11273971ae3413ffd0bcb49850
judithmf00 <judithmf00@gmail.com>
2024-05-14 17:05:32 +0200

    Django conectado con la base de datos y funcionando

--- BackEnd/iliberp/settings.py
+++ BackEnd/iliberp/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'iliberp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mi_tienda',
+        'USER': 'root',
+        'PASSWORD': 'toor',
+        'HOST': 'localhost', 
+        'PORT': '3306', 
     }
 }
+
 
 
 # Password validation
--- BackEnd/inventario/models.py
+++ BackEnd/inventario/models.py
@@ -9,6 +9,8 @@ class Producto(models.Model):
         descripcion (TextField): Descripción del producto
         precio (DecimalField): Precio del producto
         especificaciones (JSONField): Especificaciones del producto (almacenadas como JSON)
+        precio_descuento (DecimalField, opcional): Precio del producto con descuento aplicado
+        destacado (BooleanField): Indica si el producto está destacado o no
         foto (BinaryField): Foto del producto (almacenada como datos binarios)
     """
 
@@ -16,4 +18,6 @@ class Producto(models.Model):
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     especificaciones = models.JSONField()
+    precio_descuento = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    destacado = models.BooleanField(default=False)
     foto = models.BinaryField()
