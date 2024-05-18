c760e92663aac07739950cfd72acfed56ea6538f
RaulxAG <raul.arrgon@outlook.es>
2024-05-18 22:10:43 +0200

    añadido: API Inventario (obtener productos, crear pedidos)

--- BackEnd/inventario/admin.py
+++ BackEnd/inventario/admin.py
@@ -1,15 +1,26 @@
 from django.contrib import admin
 from .models import Producto, Linea, Pedido
 
-# Register your models here.
+# Inline para mostrar las líneas dentro del pedido
+class LineaInline(admin.TabularInline):
+    model = Linea
+    extra = 1
+
 @admin.register(Producto)
 class Producto(admin.ModelAdmin):
     list_display = ('nombre', 'descripcion', 'precio', 'especificaciones', 'precio_descuento', 'tipo', 'destacado', 'foto')
 
 @admin.register(Linea)
 class Linea(admin.ModelAdmin):
-    list_display = ('articulo', 'unidades')
+    list_display = ('articulo', 'unidades', 'pedido')
 
 @admin.register(Pedido)
 class Pedido(admin.ModelAdmin):
-    list_display = ('cliente', 'lineas', 'fecha', 'direccion', 'estado', 'subtotal', 'IVA', 'total')
+    list_display = ('cliente', 'fecha', 'direccion', 'estado', 'subtotal', 'IVA', 'total')
+    inlines = [LineaInline]
+
+    # (Opcional) Método para mostrar un resumen de las líneas en list_display
+    def lineas_resumen(self, obj):
+        return ", ".join([f"{linea.unidades} x {linea.articulo.nombre}" for linea in obj.lineas.all()])
+
+    lineas_resumen.short_description = 'Resumen de Líneas'
--- BackEnd/inventario/urls.py
+++ BackEnd/inventario/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     # Add more URL patterns as needed
     path("getProductsJSON/", views.getProductsJSON),
+    path("getSingleProductJSON/", views.getSingleProductJSON),
     path("getFeaturedProductsJSON/", views.getFeaturedProductsJSON),
+    path("getCategorizedProductsJSON/", views.getCategorizedProductsJSON),
+
+    path("getOrdersJSON/", views.getOrdersJSON),
+    path("makeOrderJSON/", views.makeOrderJSON),
 ]
