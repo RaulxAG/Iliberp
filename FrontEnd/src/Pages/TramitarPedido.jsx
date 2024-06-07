604cd9c4076d30d3e9b5e076113174ebd713277d
judithmf00 <judithmf00@gmail.com>
2024-06-07 19:50:16 +0200

    Acabados estilos pdf pedido

--- FrontEnd/src/Pages/TramitarPedido.jsx
+++ FrontEnd/src/Pages/TramitarPedido.jsx
@@ -9,9 +9,13 @@ import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import { Select,MenuItem } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
 import { useLocation } from 'react-router-dom';
 import PayPalComponent from "../Components/Paypal";
 import { Link } from 'react-router-dom';
+
+import { useEffect } from 'react';
 
 const steps = ['Información de envío', 'Resumen pedido', 'Pago'];
 
@@ -46,7 +50,7 @@ export default function TramitarPedido() {
           handleSubmit(onSubmit)();
         } else if (activeStep === steps.length - 1) {
           // Si es el último paso, finalizar el pedido
-          finalizarPedido();
+        //   finalizarPedido();
         } else {
           // Si no está en los pasos 0, 1 o el último, avanzar al siguiente paso
           handleNext();
@@ -74,57 +78,60 @@ export default function TramitarPedido() {
     };
 
      //------------FIN Código usado por material para ael componente------//
+    
+    //Recoger del location el carrito, que lo hemos pasado en el componente 'Carrito' mediante el link
     let location = useLocation();
     let carrito = location.state.carrito;
+
     const { register, handleSubmit, watch,formState: { errors } } = useForm();
 
-    console.log(carrito)
-    const onSubmit = (data) => {
+    const onSubmit = () => {
         //Avanzar al siguiente formulario
         handleNext();
     };
 
-    const direccion=`${watch('calle')},${watch('numero')},${watch('provincia')},${watch('localidad')},${watch('codigoPostal')}`
-
-    const finalizarPedido = () => {
-        const pedido = {
-            cliente_id: 1,  // Cambiar por el id cuando se loguee
-            direccion: direccion,  // Crear el string de la calle
-            lineas: carrito.map(producto => ({
-                articulo_id: producto.id,
-                unidades: producto.cantidad
-            }))
-        };
+    //Guardar direccion para mostrarla en el detalle y para mandarla cuando se genere pdf
+    const direccion= {
+            calle:watch('calle'),
+            numero:watch('numero'),
+            provincia:watch('provincia'),
+            localidad:watch('localidad'),
+            codigoPostal:watch('codigoPostal')
+    }
     
-        fetch('http://localhost:8000/makeOrderJSON/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(pedido)
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.message) {
-                alert('Pedido creado exitosamente. ID del pedido: ' + data.pedido_id);
-            } else {
-                alert('Error al crear el pedido');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    };
 
 
-    const [address, setAddress] = useState('');
+    //VARIABLES PARA LAS PROVINCIAS Y LAS LOCALIDADES
+    const [provincias,setProvincias]= useState(['Granada', 'Almería', 'Málaga'])
+    const [localidades,setLocalidades]= useState([])
+    const [selectedProvincia, setSelectedProvincia] = useState('');//Guardar la provincia para luego el desplegable de la localidad
 
-    const handlePlaceChanged = () => {
-        const place = props.autocomplete.getPlace();
-        setAddress(place.formatted_address);
-    };
+    //FUNCIÓN PARA OBTENER LAS PROVINCIAS
+    // useEffect(() => {
+    //     fetch('http://127.0.0.1:8000/getProvincias/', {
+    //         method: 'GET',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //     })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         setProvincias(data.prov.map(provincia => provincia.np))
+                // setSelectedProvincia(data.prov[0].np); //Seleccionar la priemra opcion para q sea la de por defector en el desplegable
+            
+    //     })
+    //     .catch(error => {
+    //         console.error('Error:', error);
+    //     });
+    // }, [])
+    console.log(provincias)
+    //FUNCIÓN PARA OBTENER LOS MUNICIPIOS (No se lanza hasta que no haya una provincia seleccionada)
 
-
+    //Para hacer pruebas
+    useEffect(() => {
+        setSelectedProvincia(provincias[0])
+    }, [])
+    
     return (
         <div className='containerPrincipal'>
             <Menu selected="incidencias"></Menu>
@@ -190,12 +197,26 @@ export default function TramitarPedido() {
                                                     {errors.numero && <span className="text-warning">{errors.numero.message}</span>}
                                                 </div>
                                                 <div className="mb-3 col-md-4 col-12">
-                                                    <TextField 
+                                                    {/* <TextField 
                                                         id="outlined-basic" 
                                                         label="Provincia" 
                                                         className='w-100'
                                                         {...register('provincia', { required: true })}
-                                                    />
+
+                                                    /> */}
+                                                    <Select
+                                                        labelId="provincia-label"
+                                                        id="provincia-select"
+                                                        value={selectedProvincia}
+                                                        // onChange={handleChange}
+                                                        label="Provincia"
+                                                        className='w-100'
+                                                    >
+                                                        {provincias.map((provincia, index) => (
+                                                            <MenuItem key={index} value={provincia}>{provincia}</MenuItem>
+                                                        ))}
+                                                    </Select>
+                                                    
                                                     {errors.provincia && <span className="text-warning">Debes indicar la provincia</span>}
                                                 </div>
                                                 <div className="mb-3 col-md-4 col-12">
@@ -260,89 +281,6 @@ export default function TramitarPedido() {
                                     )}
                                     {activeStep === 2 && (
                                         <Box>
-                                        {/* Información de pago */}
-                                        {/* <div className='containerTarjetaCredito my-3 w-75 m-auto d-flex flex-column '>
-                                            <div className="tarjetaCredito mb-3 p-3 m-auto d-md-block d-none">
-                                                <p className='h2'>{watch('numeroTarjeta') ? watch('numeroTarjeta') : '0000 0000 0000 000'}</p>
-                                                <p className='h2'>{watch('mm') && watch('yy') ? `${watch('mm')}/${watch('yy')}` : 'MM/YY'}</p>
-                                                <p>{watch('nombreTitular') ? watch('nombreTitular') : 'Nombre Titular Predeterminado'}</p>
-                                            </div>
-                                            <form action="" className='row justify-content-between'>
-                                                <div className="mb-3 col-12">
-                                                <TextField
-                                                    id="numeroTarjeta"
-                                                    label="Número de la tarjeta"
-                                                    className='w-100'
-                                                    {...register('numeroTarjeta', {
-                                                        required: 'Debes indicar el número de la tarjeta',
-                                                        pattern: {
-                                                            value: /^[0-9]{16}$/,
-                                                            message: 'El número de la tarjeta debe tener 16 dígitos',
-                                                        },
-                                                    })}
-                                                />
-                                                {errors.numeroTarjeta && <span className="text-warning">{errors.numeroTarjeta.message}</span>}
-                                                </div>
-                                                <div className="mb-3 col-12">
-                                                    <TextField
-                                                        id="nombreTitular"
-                                                        label="Nombre del titular"
-                                                        className='w-100'
-                                                        {...register('nombreTitular', { required: 'Debes indicar el nombre del titular' })}
-                                                    />
-                                                    {errors.nombreTitular && <span className="text-warning">{errors.nombreTitular.message}</span>}
-                                                </div>
-                                                <div className='col-md-8 col-12 row '>
-                                                    <div className="mb-3 col-md-4 col-12">
-                                                    <TextField
-                                                        id="mm"
-                                                        label="MM"
-                                                        className='w-100'
-                                                        {...register('mm', {
-                                                            required: 'Debes indicar el mes de vencimiento',
-                                                            pattern: {
-                                                                value: /^(0?[1-9]|1[012])$/,
-                                                                message: 'Formato inválido, introduce un mes válido (01-12)',
-                                                            },
-                                                        })}
-                                                    />
-                                                    {errors.mm && <span className="text-warning">{errors.mm.message}</span>}
-                                                    </div>
-                                                    <p className='col-1 h2 text-center d-md-block d-none'>/</p>
-                                                    <div className="mb-3 col-md-4 col-12">
-                                                    <TextField
-                                                        id="yy"
-                                                        label="YY"
-                                                        className='w-100'
-                                                        {...register('yy', {
-                                                            required: 'Debes indicar el año de vencimiento',
-                                                            pattern: {
-                                                                value: /^(2[2-9]|[3-9][0-9])$/,
-                                                                message: 'Formato inválido, introduce un año válido (22-99)',
-                                                            },
-                                                        })}
-                                                    />
-                                                    {errors.yy && <span className="text-warning">{errors.yy.message}</span>}
-                                                    </div>
-                                                </div>
-                                                
-                                                <div className="mb-3 col-md-4 col-12">
-                                                <TextField
-                                                    id="cvv"
-                                                    label="CVV"
-                                                    className='w-100'
-                                                    {...register('cvv', {
-                                                        required: 'Debes indicar el código CVV',
-                                                        pattern: {
-                                                            value: /^[0-9]{3}$/,
-                                                            message: 'El código CVV debe tener 3 dígitos',
-                                                        },
-                                                    })}
-                                                />
-                                                {errors.cvv && <span className="text-warning">{errors.cvv.message}</span>}
-                                                </div>
-                                            </form>
-                                        </div> */}
                                             <PayPalComponent  monto={carrito.reduce((total, producto) => total + producto.precio * producto.cantidad, 0)}/>
                                         </Box>
                                     )}
